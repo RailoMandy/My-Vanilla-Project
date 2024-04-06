d4939541dffaa8506a6d72d5960779c0ab4c715c
RailoMandy <mandyrcm00@gmail.com>
2024-04-07 00:07:46 +0200

    inserting time and date functions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,37 @@ function refreshWeather(response){
 let temperatureElement=document.querySelector("#temperature");
 let temperature=response.data.temperature.current;
 let cityElement = document.querySelector("#city");
-let descriptionElement=document.querySelector("description");
+let descriptionElement=document.querySelector("#description");
+let humidityElement=document.querySelector("#humidity");
+let windSpeedElement=document.querySelector("#wind-speed")
+let timeElement=document.querySelector("#time")
+let date=new date(response.data.time*1000)
 
-
+timeElement.innerHTML=formatDate();
 cityElement.innerHTML = response.data.city;
 descriptionElement.innerHTML=response.data.condition.description;
 temperatureElement.innerHTML = Math.round(temperature);
+humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+windSpeedElement.innerHTML=`${response.data.wind.speed}m/s`;
+
+}
+
+function formatDate(date){
+    let minutes=date.getMinutes();
+    let hours=date.getHours();
+    let days=[
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "ThursDay",
+        "Friday",
+        "Saturday"];
+ let day=day[date.getDay()];
+ if (minutes<10){minutes=`0${minutes}`
+}
+
+ return`${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city){

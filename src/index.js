16b2d799878769548cccd9050bb0be7ff2cab968
RailoMandy <mandyrcm00@gmail.com>
2024-04-19 11:39:18 +0200

    debugged javascript

--- src/index.js
+++ src/index.js
@@ -58,6 +58,13 @@ function getForecast(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 
 function displayForecast(response) {
   console.log(response);

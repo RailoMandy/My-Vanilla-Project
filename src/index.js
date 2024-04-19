5b3249bbba67aeeb454a290d2a9b0b65f9162ede
RailoMandy <mandyrcm00@gmail.com>
2024-04-19 15:16:49 +0200

    updated css,fixed the forecast icons to align with day&temp
    included icons for humidity and wind and aligned forecast elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+function searchCity(city) {
+  let apiKey = "7e00d8f1t71e42efc318143acbd6e2oa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios
+    .get(apiUrl)
+    .then(function (response) {
+      if (response.data.city) {
+        refreshWeather(response);
+      } else {
+        console.error("Invalid city name:", city);
+        displayErrorMessage();
+      }
+    })
+    .catch(function (error) {
+      console.error("Error fetching weather data:", error);
+      displayErrorMessage();
+    });
+}
+
+function displayErrorMessage() {
+  temperatureElement.innerHTML = "";
+  cityElement.innerHTML = "Invalid city name";
+  descriptionElement.innerHTML = "";
+  humidityElement.innerHTML = "";
+  windSpeedElement.innerHTML = "";
+  iconElement.innerHTML = "";
+  timeElement.innerHTML = "";
+}
+
 function refreshWeather(response) {
   console.log(response);
   let temperatureElement = document.querySelector("#temperature");
@@ -15,7 +45,10 @@ function refreshWeather(response) {
   timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
-  temperatureElement.innerHTML = Math.round(temperature);
+  temperatureElement.innerHTML = `${Math.round(
+    temperature
+  )}<span class="current-temperature-unit">°C</span>`;
+
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}m/s`;
 
@@ -39,12 +72,6 @@ function formatDate(date) {
     minutes = `0${minutes}`;
   }
   return `${day} ${hours}:${minutes}`;
-}
-
-function searchCity(city) {
-  let apiKey = "7e00d8f1t71e42efc318143acbd6e2oa";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
@@ -95,3 +122,6 @@ function displayForecast(response) {
 
 let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+let searchButton = document.querySelector(".search-form-button");
+searchButton.addEventListener("click", handleSearchSubmit);
